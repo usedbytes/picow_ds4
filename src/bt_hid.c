ffc50b5500ee8f84f9b2cc14a5600fe5ac0f4a30
Brian Starkey <stark3y@gmail.com>
2023-05-14 21:53:06 +0100

    Add MAC address for my knockoff DS4
    Looks like the pairing is persistent (thanks to the link key database I
    assume), so now both controllers are paired, either can connect.

--- src/bt_hid.c
+++ src/bt_hid.c
@@ -59,7 +59,9 @@
 // SN30 Pro
 //static const char * remote_addr_string = "E4:17:D8:EE:73:0E";
 // Real DS4
-static const char * remote_addr_string = "00:22:68:DB:D3:66";
+//static const char * remote_addr_string = "00:22:68:DB:D3:66";
+// Knockoff DS4
+static const char * remote_addr_string = "A5:15:66:8E:91:3B";
 
 static bd_addr_t remote_addr;
 static bd_addr_t connected_addr;
